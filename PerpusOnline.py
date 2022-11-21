a5a72bc7cf6acf29796ee5ef0e27f1348ec34130
Iqbaleleo132 <iqbalmaulan056@gmail.com>
2022-11-21 08:41:51 +0700

    Projek Perpus Online Python

---
+++ PerpusOnline.py
@@ -0,0 +1,22 @@
+def garis():
+    print("======================================================")
+
+def menu():
+    print("   ~~~~~SELAMAT DATANG DI PERPUSTAKAAN ONLINE~~~~~")
+    garis()
+    print("Pilih daftar menu dibawah ini untuk mengakses program :\n")
+    print("[1] Daftar Buku")
+    print("[2] Cari Buku")
+    print("[3] Tambah Data Buku")
+    print("[4] Ubah Data Buku")
+    print("[5] Hapus Data Buku")
+    print("[6] Lihat Daftar Peminjaman Buku")
+    print("[7] Tambah Peminjaman Buku")
+    print("[8] Hapus Data Peminjaman Buku")
+    print("[9] Keluar")
+
+    kode = int(input("\nMasukan Kode Yang  Ingin Di Akses : "))
+
+menu()
+
+
